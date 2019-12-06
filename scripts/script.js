9fc3cae308edc43e4ff25b4c0493404a13863e1b
Gopikan Jeysankar <gopikan.jeysankar@csbe.checho>
2019-12-06 16:08:02 +0100

    uek101

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+
+// handburger nav click anime 
+$("#barcontainer").click(function(){
+    $(this).toggleClass("change");
+    $("#responsivenav").toggleClass("responsive");
+});
+// handburger nav click anime END
